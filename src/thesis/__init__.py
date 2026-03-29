fee4752e3533b8fa658bd547ae264697e01889c2
Hieu Nguyen <hieuteo03@gmail.com>
2026-03-29 17:19:27 +0700

    feat: add core infrastructure and config loader

---
+++ src/thesis/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Hybrid Stacking (LSTM + LightGBM) for XAU/USD H1 Trading Signals
+Bachelor's Thesis - Thuy Loi University
+Nguyen Duc Hieu - 2151061192
+"""
+
+__version__ = "0.1.0"
+__author__ = "Nguyen Duc Hieu"
