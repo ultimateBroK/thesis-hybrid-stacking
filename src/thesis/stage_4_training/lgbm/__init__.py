696d305feb3042e288c88a002ff191e1ae03a91d
ultimateBroK <ultimateBroK@users.noreply.github.com>
2026-05-14 23:24:06 +0700

    refactor(stage4 lgbm): simplify package docstring

--- src/thesis/stage_4_training/lgbm/__init__.py
+++ src/thesis/stage_4_training/lgbm/__init__.py
@@ -1,7 +1,1 @@
-"""LightGBM training sub-package."""
-
-from thesis.stage_4_training.walk_forward.lgbm import (
-    train_lgbm_walk_forward as train_model,
-)
-
-__all__ = ["train_model"]
+"""LightGBM sub-package."""
